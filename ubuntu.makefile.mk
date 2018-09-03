b10decaed66cc57b5e4703e513b015e06860cfea
Guido Draheim <guidod@gmx.de>
2018-09-03 20:57:55 +0200

    fix 'apt-get update' not getting the latest repo

--- ubuntu.makefile.mk
+++ ubuntu.makefile.mk
@@ -7,6 +7,8 @@ UBUNTU_OS=ubuntu
 UBUNTU=16.04
 RSYNC_UBUNTU=rsync://ftp5.gwdg.de/pub/linux/debian/ubuntu
 
+UBUNTU_TMP = ubuntu.tmp
+
 UBUNTU18.10 = cosmic
 UBUNTU18.04 = bionic
 UBUNTU17.10 = artful
@@ -21,7 +23,26 @@ ubuntu:
 
 ubuntusync:
 	$(MAKE) ubuntudir
-	$(MAKE) ubuntu-sync-main # ubuntu-sync-restricted ubuntu-sync-universe ubuntu-sync-multiverse
+	$(MAKE) ubuntu-sync-base.1
+	$(MAKE) ubuntu-sync-base.2
+	$(MAKE) ubuntu-sync-base.3
+	$(MAKE) ubuntu-sync-base.4
+	$(MAKE) ubuntu-sync-main.1
+	$(MAKE) ubuntu-sync-restricted.1
+	$(MAKE) ubuntu-sync-universe.1
+	$(MAKE) ubuntu-sync-multiverse.1
+	$(MAKE) ubuntu-sync-main.2
+	$(MAKE) ubuntu-sync-restricted.2
+	$(MAKE) ubuntu-sync-universe.2
+	$(MAKE) ubuntu-sync-multiverse.2
+	$(MAKE) ubuntu-sync-main.3
+	$(MAKE) ubuntu-sync-restricted.3
+	$(MAKE) ubuntu-sync-universe.3
+	$(MAKE) ubuntu-sync-multiverse.3
+	$(MAKE) ubuntu-sync-main.4
+	$(MAKE) ubuntu-sync-restricted.4
+	$(MAKE) ubuntu-sync-universe.4
+	$(MAKE) ubuntu-sync-multiverse.4
 ubuntudir:
 	@ test ! -d ubuntu.$(UBUNTU) || rmdir -v ubuntu.$(UBUNTU) || rm -v ubuntu.$(UBUNTU)
 	@ for data in $(UBUNTUDATADIRS); do : \
@@ -34,32 +55,66 @@ ubuntudir:
 	@ if test -d ubuntu.$(UBUNTU)/. ; then : \
 	; else mkdir -v ubuntu.$(UBUNTU) ; fi
 	ls -ld ubuntu.$(UBUNTU)
-ubuntu-sync-main:
-	echo "UBUNTU$(UBUNTU) = $(UBUNTU$(UBUNTU))"
-	test  -d ubuntu.$(UBUNTU)/dists/$(UBUNTU$(UBUNTU))/main || \
-	mkdir -p ubuntu.$(UBUNTU)/dists/$(UBUNTU$(UBUNTU))/main 
-	rsync -rv $(RSYNC_UBUNTU)/dists/$(UBUNTU$(UBUNTU))/main/binary-amd64 \
-	         ubuntu.$(UBUNTU)/dists/$(UBUNTU$(UBUNTU))/main \
-	   --size-only --filter="exclude *.src.rpm"
-	rsync -rv $(RSYNC_UBUNTU)/dists/$(UBUNTU$(UBUNTU))/main/binary-i386 \
-	         ubuntu.$(UBUNTU)/dists/$(UBUNTU$(UBUNTU))/main \
-	   --size-only --filter="exclude *.src.rpm"
-	zcat ubuntu.$(UBUNTU)/dists/$(UBUNTU$(UBUNTU))/main/binary-amd64/Packages.gz \
-	     ubuntu.$(UBUNTU)/dists/$(UBUNTU$(UBUNTU))/main/binary-i386/Packages.gz \
-	| sed -e '/^Filename:/!d' -e 's|Filename: *pool/||' > Packages.$@.tmp
+ubuntu-sync-base.1: ; $(MAKE) ubuntu-sync.base dist=$(UBUNTU$(UBUNTU))
+ubuntu-sync-base.2: ; $(MAKE) ubuntu-sync.base dist=$(UBUNTU$(UBUNTU))-updates
+ubuntu-sync-base.3: ; $(MAKE) ubuntu-sync.base dist=$(UBUNTU$(UBUNTU))-backports
+ubuntu-sync-base.4: ; $(MAKE) ubuntu-sync.base dist=$(UBUNTU$(UBUNTU))-security
+ubuntu-sync.base:
+	echo "dist = $(dist)"
+	test  -d ubuntu.$(UBUNTU)/dists/$(dist) || \
+	mkdir -p ubuntu.$(UBUNTU)/dists/$(dist)
+	@ test -d $(UBUNTU_TMP) || mkdir $(UBUNTU_TMP)
+	{ echo "Release" ; echo "InRelease"; } > $(UBUNTU_TMP)/Release.$(dist).base.tmp
+	rsync -v $(RSYNC_UBUNTU)/dists/$(dist) \
+	        ubuntu.$(UBUNTU)/dists/$(dist) \
+	             --ignore-times --files-from=$(UBUNTU_TMP)/Release.$(dist).base.tmp
+ubuntu-sync-main.1:       ; $(MAKE) ubuntu-sync.main dist=$(UBUNTU$(UBUNTU)) main=main
+ubuntu-sync-restricted.1: ; $(MAKE) ubuntu-sync.main dist=$(UBUNTU$(UBUNTU)) main=restricted skip=$(SKIP)
+ubuntu-sync-universe.1:   ; $(MAKE) ubuntu-sync.main dist=$(UBUNTU$(UBUNTU)) main=universe skip=$(SKIP)
+ubuntu-sync-multiverse.1: ; $(MAKE) ubuntu-sync.main dist=$(UBUNTU$(UBUNTU)) main=multiverse skip=true
+ubuntu-sync-main.2:       ; $(MAKE) ubuntu-sync.main dist=$(UBUNTU$(UBUNTU))-updates main=main
+ubuntu-sync-restricted.2: ; $(MAKE) ubuntu-sync.main dist=$(UBUNTU$(UBUNTU))-updates main=restricted skip=$(SKIP)
+ubuntu-sync-universe.2:   ; $(MAKE) ubuntu-sync.main dist=$(UBUNTU$(UBUNTU))-updates main=universe skip=$(SKIP)
+ubuntu-sync-multiverse.2: ; $(MAKE) ubuntu-sync.main dist=$(UBUNTU$(UBUNTU))-updates main=multiverse skip=true
+ubuntu-sync-main.3:       ; $(MAKE) ubuntu-sync.main dist=$(UBUNTU$(UBUNTU))-backports main=main
+ubuntu-sync-restricted.3: ; $(MAKE) ubuntu-sync.main dist=$(UBUNTU$(UBUNTU))-backports main=restricted  skip=$(SKIP)
+ubuntu-sync-universe.3:   ; $(MAKE) ubuntu-sync.main dist=$(UBUNTU$(UBUNTU))-backports main=universe skip=$(SKIP)
+ubuntu-sync-multiverse.3: ; $(MAKE) ubuntu-sync.main dist=$(UBUNTU$(UBUNTU))-backports main=multiverse skip=true
+ubuntu-sync-main.4:       ; $(MAKE) ubuntu-sync.main dist=$(UBUNTU$(UBUNTU))-security main=main
+ubuntu-sync-restricted.4: ; $(MAKE) ubuntu-sync.main dist=$(UBUNTU$(UBUNTU))-security main=restricted  skip=$(SKIP)
+ubuntu-sync-universe.4:   ; $(MAKE) ubuntu-sync.main dist=$(UBUNTU$(UBUNTU))-security main=universe skip=$(SKIP)
+ubuntu-sync-multiverse.4: ; $(MAKE) ubuntu-sync.main dist=$(UBUNTU$(UBUNTU))-security main=multiverse skip=true
+
+ubuntu-sync.main:
+	test  -d ubuntu.$(UBUNTU)/dists/$(dist)/$(main) || \
+	mkdir -p ubuntu.$(UBUNTU)/dists/$(dist)/$(main)
+	rsync -rv $(RSYNC_UBUNTU)/dists/$(dist)/$(main)/binary-amd64 \
+	         ubuntu.$(UBUNTU)/dists/$(dist)/$(main) --ignore-times
+	rsync -rv $(RSYNC_UBUNTU)/dists/$(dist)/$(main)/binary-i386 \
+	         ubuntu.$(UBUNTU)/dists/$(dist)/$(main) --ignore-times
+	rsync -rv $(RSYNC_UBUNTU)/dists/$(dist)/$(main)/source \
+	         ubuntu.$(UBUNTU)/dists/$(dist)/$(main) --ignore-times
+	@ test -d $(UBUNTU_TMP) || mkdir $(UBUNTU_TMP)
+	zcat ubuntu.$(UBUNTU)/dists/$(dist)/$(main)/binary-amd64/Packages.gz \
+	     ubuntu.$(UBUNTU)/dists/$(dist)/$(main)/binary-i386/Packages.gz \
+	| sed -e '/^Filename:/!d' -e 's|Filename: *pool/||' \
+	                          > $(UBUNTU_TMP)/Packages.$(dist).$(main).tmp
+	test -n "$(skip)" || \
 	rsync -rv $(RSYNC_UBUNTU)/pool \
 	         ubuntu.$(UBUNTU)/pool \
-	   --size-only --files-from=Packages.$@.tmp
+	   --size-only --files-from=$(UBUNTU_TMP)/Packages.$(dist).$(main).tmp
 
 ubunturepo_CMD = ["python","/srv/scripts/filelist.py","--data","/srv/repo"]
 ubunturepo_PORT = 80
 ubunturepo:
 	- docker rm --force $@
 	docker run --name=$@ --detach $(UBUNTU_OS):$(UBUNTU) sleep 9999
-	docker exec $@ mkdir -p /srv/repo/
+	docker exec $@ mkdir -p /srv/repo/ubuntu
+	docker exec $@ mkdir -p /srv/repo/ubuntu
 	docker cp scripts $@:/srv/scripts
-	docker cp ubuntu.$(UBUNTU)/dists $@:/srv/repo/
-	docker cp ubuntu.$(UBUNTU)/pool  $@:/srv/repo/
+	docker cp ubuntu.$(UBUNTU)/dists $@:/srv/repo/ubuntu
+	docker cp ubuntu.$(UBUNTU)/pool  $@:/srv/repo/ubuntu
+	docker exec $@ apt-get update
 	docker exec $@ apt-get install -y python
 	docker commit -c 'CMD $($@_CMD)' -c 'EXPOSE $($@_PORT)' $@ $(IMAGESREPO)/ubuntu-repo:$(UBUNTU)
 	docker rm --force $@
