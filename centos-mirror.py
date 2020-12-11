b8b73e4b8b955532afcceaeb7f19852071f3e875
Guido Draheim <guidod@gmx.de>
2020-12-11 09:27:03 +0100

    1.6.2495

--- centos-mirror.py
+++ centos-mirror.py
@@ -10,7 +10,7 @@
 __copyright__ = "(C) 2020 Guido Draheim"
 __contact__ = "https://github.com/gdraheim/docker-mirror-packages-repo"
 __license__ = "CC0 Creative Commons Zero (Public Domain)"
-__version__ = "1.6.2494"
+__version__ = "1.6.2495"
 
 # from __future__ import literal_string_interpolation # PEP498 Python3.6
 from typing import Optional, Dict, List, Tuple, Union
