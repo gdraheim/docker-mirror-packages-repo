b0e9326a91892973d62d8d29b5bac66cff925d44
Guido Draheim <guidod@gmx.de>
2020-12-16 08:16:45 +0100

    v1.6.2503

--- scripts/filelist.py
+++ scripts/filelist.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 __copyright__ = "(C) 2018-2020 Guido Draheim"
 __contact__ = "https://github.com/gdraheim/docker-mirror-packages-repo"
 __license__ = "CC0 Creative Commons Zero (Public Domain)"
-__version__ = "1.6.2501"
+__version__ = "1.6.2503"
 
 import optparse
 import os
--- ubuntu-mirror.py
+++ ubuntu-mirror.py
@@ -8,7 +8,7 @@
 __copyright__ = "(C) 2020 Guido Draheim"
 __contact__ = "https://github.com/gdraheim/docker-mirror-packages-repo"
 __license__ = "CC0 Creative Commons Zero (Public Domain)"
-__version__ = "1.6.2501"
+__version__ = "1.6.2503"
 
 # from __future__ import literal_string_interpolation # PEP498 Python3.6
 from typing import Optional, Dict, List, Tuple, Union
