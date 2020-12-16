0914963056d8ad80163547583c674b41a02bf1ff
Guido Draheim <guidod@gmx.de>
2020-12-16 07:35:34 +0100

    epel 8

--- epel-makefile.mk
+++ epel-makefile.mk
@@ -1,12 +1,14 @@
 #! /usr/bin/make -f
 
+EPEL = 8
+
 epel:
 	$(MAKE) epeldir
 	$(MAKE) epelsync
 	$(MAKE) epelrepo
 
-epeldir:  ; ./centos-mirror.py 7 epeldir
-epelsync: ; ./centos-mirror.py 7 epelsync
-epelrepo: ; ./centos-mirror.py 7 epelrepo
+epeldir:  ; ./centos-mirror.py $(EPEL) epeldir
+epelsync: ; ./centos-mirror.py $(EPEL) epelsync
+epelrepo: ; ./centos-mirror.py $(EPEL) epelrepo
 
 # centos:8.0.1905 does refer to epel-repo:7, so until the end of 2019 we tag it also as epel-repo:8
