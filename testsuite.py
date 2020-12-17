ed37cfc8d7463ae6cb92606a5c1d18303083d9b1
Guido Draheim <guidod@gmx.de>
2020-12-17 14:53:28 +0100

    v1.6.2504

--- testsuite.py
+++ testsuite.py
@@ -7,7 +7,7 @@
 __copyright__ = "(C) 2018-2020 Guido Draheim"
 __contact__ = "https://github.com/gdraheim/docker-mirror-packages-repo"
 __license__ = "CC0 Creative Commons Zero (Public Domain)"
-__version__ = "1.6.2503"
+__version__ = "1.6.2504"
 
 import sys
 import subprocess
