af3bda4f1b46bdd4cb37ee4035eb8fd28eea0726
Guido Draheim <guidod@gmx.de>
2023-03-13 00:32:50 +0100

    v1.7.5111

--- docker_mirror.tests.py
+++ docker_mirror.tests.py
@@ -7,7 +7,7 @@
 __copyright__ = "(C) 2018-2023 Guido Draheim"
 __contact__ = "https://github.com/gdraheim/docker-mirror-packages-repo"
 __license__ = "CC0 Creative Commons Zero (Public Domain)"
-__version__ = "1.6.5107"
+__version__ = "1.7.5111"
 
 from typing import Union, Optional, List, cast
 import sys
--- scripts/filelist.py
+++ scripts/filelist.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 __copyright__ = "(C) 2018-2023 Guido Draheim"
 __contact__ = "https://github.com/gdraheim/docker-mirror-packages-repo"
 __license__ = "CC0 Creative Commons Zero (Public Domain)"
-__version__ = "1.6.5107"
+__version__ = "1.7.5111"
 
 import optparse
 import os
