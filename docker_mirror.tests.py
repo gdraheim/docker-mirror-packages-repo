b5a9edb5a8deb9e466a306b2ec8de3071cf79c9b
Guido Draheim <guidod@gmx.de>
2025-01-15 23:53:51 +0100

    v1.7.7023

--- docker_mirror.tests.py
+++ docker_mirror.tests.py
@@ -7,7 +7,7 @@
 __copyright__ = "(C) 2018-2025 Guido Draheim"
 __contact__ = "https://github.com/gdraheim/docker-mirror-packages-repo"
 __license__ = "CC0 Creative Commons Zero (Public Domain)"
-__version__ = "1.7.7021"
+__version__ = "1.7.7023"
 
 from typing import Union, Optional, List, Dict, cast
 import sys
