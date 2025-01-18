5a3e32b2e00357f21baf678a5f9db566602daf6b
Guido Draheim <guidod@gmx.de>
2025-01-18 16:21:35 +0100

    v1.7.7026

--- ubuntu-docker-mirror.py
+++ ubuntu-docker-mirror.py
@@ -8,7 +8,7 @@
 __copyright__ = "(C) 2025 Guido Draheim"
 __contact__ = "https://github.com/gdraheim/docker-mirror-packages-repo"
 __license__ = "CC0 Creative Commons Zero (Public Domain)"
-__version__ = "1.7.7023"
+__version__ = "1.7.7026"
 
 # from __future__ import literal_string_interpolation # PEP498 Python3.6
 from typing import Optional, Dict, List, Tuple, Union
