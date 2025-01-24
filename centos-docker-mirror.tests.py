dd14ac1bf1a0573f04f2f16c5b2604a570eaa2c9
Guido Draheim <guidod@gmx.de>
2025-01-24 01:55:01 +0100

    v1.7.7034

--- centos-docker-mirror.tests.py
+++ centos-docker-mirror.tests.py
@@ -2,7 +2,7 @@
 __copyright__ = "(C) 2025 Guido Draheim"
 __contact__ = "https://github.com/gdraheim/docker-mirror-packages-repo"
 __license__ = "CC0 Creative Commons Zero (Public Domain)"
-__version__ = "1.7.7026"
+__version__ = "1.7.7034"
 
 # pylint: disable=unused-import,line-too-long,too-many-lines
 from typing import Optional, Union, Dict, List, Any, Sequence, Callable, Iterable, cast, NamedTuple
--- docker_mirror.py
+++ docker_mirror.py
@@ -4,7 +4,7 @@
 __copyright__ = "(C) 2025 Guido Draheim"
 __contact__ = "https://github.com/gdraheim/docker-mirror-packages-repo"
 __license__ = "CC0 Creative Commons Zero (Public Domain)"
-__version__ = "1.7.7026"
+__version__ = "1.7.7034"
 
 from collections import OrderedDict, namedtuple
 import os.path
