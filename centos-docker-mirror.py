dc88206d0ea8050e43c78a2ab93822bd7c2f9d3a
Guido Draheim <guidod@gmx.de>
2025-03-25 07:46:32 +0100

    v1.7.7121

--- centos-docker-mirror.py
+++ centos-docker-mirror.py
@@ -11,7 +11,7 @@
 __copyright__ = "(C) 2025 Guido Draheim"
 __contact__ = "https://github.com/gdraheim/docker-mirror-packages-repo"
 __license__ = "CC0 Creative Commons Zero (Public Domain)"
-__version__ = "1.7.7112"
+__version__ = "1.7.7121"
 
 from typing import Optional, Dict, List, Tuple, Union, Any, Set
 from collections import OrderedDict
--- dockerdir.py
+++ dockerdir.py
@@ -42,7 +42,7 @@ from __future__ import division
  """
 
 __copyright__ = """ (c) 2015-2022 Guido Draheim """
-__version__ = "1.7.7112"
+__version__ = "1.7.7121"
 import sys
 import re
 import os
